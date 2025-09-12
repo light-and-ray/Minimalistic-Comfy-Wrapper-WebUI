b0316692347aeff7d1afdae3fca89d0c7e2cef5e
Andray <light.and.ray@gmail.com>
2025-09-12 16:06:05 +0400

    remove default of COMFY_WORKFLOWS_PATH

--- src/settings.py
+++ src/settings.py
@@ -2,11 +2,11 @@ import os, dotenv
 import gradio as gr
 
 dotenv.load_dotenv(os.path.join("..", ".env"))
+os.chdir("..")
 
 COMFY_ADDRESS = os.getenv("COMFY_ADDRESS", "localhost:8188")
 COMFY_ADDRESS = COMFY_ADDRESS.lower().removesuffix('/').removeprefix("http://")
-COMFY_WORKFLOWS_PATH = os.getenv("COMFY_WORKFLOWS_PATH", "workflows/")
-COMFY_WORKFLOWS_PATH = os.path.join("..", COMFY_WORKFLOWS_PATH)
+COMFY_WORKFLOWS_PATH = os.getenv("COMFY_WORKFLOWS_PATH", "")
 COMFY_WORKFLOWS_PATH = os.path.realpath(COMFY_WORKFLOWS_PATH)
 WEBUI_TITLE = os.getenv("WEBUI_TITLE", "Minimalistic Comfy Wrapper WebUI")
 
