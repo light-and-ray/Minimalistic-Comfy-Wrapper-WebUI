fec0bca2be9b0e2c07d8967682c4d1c01daf3c46
Andray <light.and.ray@gmail.com>
2025-09-12 19:40:12 +0400

    little fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 window.addEventListener('beforeunload', (event) => {
-    // event.preventDefault();
-    event.returnValue = "";
+    event.preventDefault();
+    event.returnValue = "Are you sure";
 });
 
 [...document.getElementsByClassName('cm-content')].forEach(elem => elem.setAttribute('spellcheck', 'true'));
