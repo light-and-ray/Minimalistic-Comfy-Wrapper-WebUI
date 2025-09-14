48679e558b686f93d32cd7bfdca3379c0386dbc5
Andray <light.and.ray@gmail.com>
2025-09-14 21:41:07 +0400

    add start.cmd for windows

---
+++ start.cmd
@@ -0,0 +1,24 @@
+:: This script is blindly translated from Linux using LLM.
+:: If it contains errors, please make an issue
+
+@echo off
+setlocal
+
+:: Change to script directory
+cd /d "%~dp0"
+
+:: Activate virtual environment
+call venv\Scripts\activate.bat
+
+:: Enable verbose output
+@echo on
+
+:: Change to src directory
+cd src
+
+:: Run python script with all arguments
+python main.py %*
+
+endlocal
+
+pause
