b228a05a0d8b2c2a895f7bf47748e6a59c87911e
Andray <light.and.ray@gmail.com>
2025-10-07 08:48:02 +0400

    little fix

--- mcww_web/saveState.js
+++ mcww_web/saveState.js
@@ -3,10 +3,18 @@ const BUTTON_SELECTOR = '.save_states';
 const MAX_WAIT_MS = 1000;
 let saveStateInProgress = false;
 
+function afterStateSaved(...args) {
+    saveStateInProgress = false;
+}
+
 async function waitForSave() {
     const startTime = Date.now();
     while (saveStateInProgress && (Date.now() - startTime < MAX_WAIT_MS)) {
         await new Promise(resolve => setTimeout(resolve, 10));
+    }
+    if (saveStateInProgress) {
+        console.warn(`[${new Date().toLocaleTimeString()}] Save operation timed out`);
+        saveStateInProgress = false;
     }
 }
 
@@ -23,25 +31,13 @@ async function doSaveStates(...args) {
         try {
             // console.log(`[${new Date().toLocaleTimeString()}] Clicking save button...`);
             saveStatesButton.click();
-
-            // Wait until saveStateInProgress is set to false, but with timeout
             await waitForSave();
-
-            if (saveStateInProgress) {
-                console.warn(`[${new Date().toLocaleTimeString()}] Save operation timed out`);
-                saveStateInProgress = false; // Force reset if timed out
-            } else {
-                // console.log(`[${new Date().toLocaleTimeString()}] Save completed, ready for next interval`);
-            }
         } catch (error) {
             console.error(`[${new Date().toLocaleTimeString()}] Error clicking the button:`, error);
-            saveStateInProgress = false; // Reset in case of error
+            saveStateInProgress = false;
         }
     }
 }
 
 const intervalId = setInterval(doSaveStates, AUTO_SAVE_STATE_MS);
 
-function afterStateSaved(...args) {
-    saveStateInProgress = false;
-}
