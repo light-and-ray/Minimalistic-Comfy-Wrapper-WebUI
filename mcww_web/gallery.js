923aa925864bfdf87924ce8cd0b741aa31f011bb
Andray <light.and.ray@gmail.com>
2025-10-10 21:24:47 +0400

    clean up gallery fullscreen clicks

--- mcww_web/gallery.js
+++ mcww_web/gallery.js
@@ -15,44 +15,61 @@ function fixGalleries() {
 onUiUpdate(fixGalleries);
 
 function attachFullscreenHandlers(element, container) {
-    element.addEventListener('click', () => {
+    const clickHandler = () => {
         const fullscreenButton = container.querySelector('button[title="Fullscreen"]');
         if (fullscreenButton) {
             fullscreenButton.click();
         }
-    });
-    element.addEventListener('dblclick', () => {
+    };
+    const dblClickHandler = () => {
         const fullscreenButton = container.querySelector('button[title="Exit fullscreen mode"]');
         if (fullscreenButton) {
             fullscreenButton.click();
         }
-    });
+    };
+
+    element.addEventListener('click', clickHandler);
+    element.addEventListener('dblclick', dblClickHandler);
+
+    element._fullscreenHandlers = { clickHandler, dblClickHandler };
+}
+
+function detachFullscreenHandlers(element) {
+    if (element._fullscreenHandlers) {
+        element.removeEventListener('click', element._fullscreenHandlers.clickHandler);
+        element.removeEventListener('dblclick', element._fullscreenHandlers.dblClickHandler);
+        delete element._fullscreenHandlers;
+    }
 }
 
 function attachFullscreenClick() {
     const galleryContainers = document.querySelectorAll('.gallery-container');
-
     galleryContainers.forEach(container => {
         const previewButton = container.querySelector('button.preview');
         const thumbnailItems = container.querySelectorAll('button.thumbnail-small');
+        const mediaButton = container.querySelector('button.media-button');
 
-        if (previewButton && thumbnailItems.length === 1) {
-            const mediaButton = container.querySelector('button.media-button');
-
-            if (mediaButton) {
-                if (!mediaButton.dataset.fullscreenClickAttached) {
-                    thumbnailItems[0].parentElement.style.display = "none";
-                    mediaButton.classList.add("mcww-full-screen-media-button");
-                    attachFullscreenHandlers(mediaButton, container);
-                    mediaButton.dataset.fullscreenClickAttached = 'true';
-                }
+        // Clean up if there are more than 1 thumbnail items
+        if (thumbnailItems.length > 1 && mediaButton && mediaButton.dataset.fullscreenClickAttached) {
+            detachFullscreenHandlers(mediaButton);
+            mediaButton.classList.remove("mcww-full-screen-media-button");
+            thumbnailItems[0].parentElement.style.display = "";
+            delete mediaButton.dataset.fullscreenClickAttached;
+        }
+        // Attach if there's exactly 1 thumbnail item
+        else if (previewButton && thumbnailItems.length === 1 && mediaButton) {
+            if (!mediaButton.dataset.fullscreenClickAttached) {
+                thumbnailItems[0].parentElement.style.display = "none";
+                mediaButton.classList.add("mcww-full-screen-media-button");
+                attachFullscreenHandlers(mediaButton, container);
+                mediaButton.dataset.fullscreenClickAttached = 'true';
             }
         }
     });
+
     const imageContainers = document.querySelectorAll('.image-container');
     imageContainers.forEach(container => {
         const images = container.querySelectorAll('img');
-
         if (images.length === 1) {
             const image = images[0];
             if (!image.dataset.fullscreenClickAttached) {
@@ -61,7 +78,6 @@ function attachFullscreenClick() {
             }
         }
     });
-
 }
 
 onUiUpdate(attachFullscreenClick);
