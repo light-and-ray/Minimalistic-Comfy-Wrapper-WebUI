e26ea74bc144377d276624ec47eb308888733fa2
Andray <light.and.ray@gmail.com>
2025-11-24 18:42:47 +0400

    move code from others.js into helpers.js

--- mcww_web/js/misc/others.js
+++ mcww_web/js/misc/others.js
@@ -1,13 +1,4 @@
 
-function scrollToComfyLogsBottom() {
-    const logsElement = document.querySelector(".comfy-logs-code");
-    if (!logsElement) return;
-    logsElement.scrollTo({
-        top: logsElement.scrollHeight,
-        behavior: 'smooth'
-    });
-}
-
 onUiUpdate(() => {
     const tabButtons = document.querySelectorAll('.project-media-prompt-tabs .tab-container button[role="tab"]');
     const tabPanels = document.querySelectorAll('.project-media-prompt-tabs div[role="tabpanel"]');
@@ -23,36 +14,3 @@ onUiUpdate(() => {
     });
 });
 
-
-var needHideHelpersInfo = false;
-
-function updateHelpersInfo() {
-    const updateInfoButton = document.querySelector('button.mcww-update-helpers-info-button')
-    if (updateInfoButton && uiElementIsVisible(updateInfoButton.parentElement)) {
-        const overflowMenu = document.querySelector('.overflow-dropdown');
-        if (!overflowMenu || !uiElementIsVisible(overflowMenu)) {
-            updateInfoButton.click();
-            needHideHelpersInfo = true;
-        }
-    } else {
-        const showButton = document.querySelector('button.mcww-show-helpers-info-button');
-        const hideButton = document.querySelector('button.mcww-hide-helpers-info-button');
-        const row = showButton?.parentElement;
-        if (row) {
-            if (!uiElementIsVisible(row)) {
-                if (hideButton && needHideHelpersInfo) {
-                    hideButton.click();
-                    needHideHelpersInfo = false;
-                }
-            } else {
-                if (showButton) {
-                    showButton.click()
-                    needHideHelpersInfo = false;
-                }
-            }
-        }
-    }
-    setTimeout(updateHelpersInfo, 1000);
-}
-
-updateHelpersInfo();
