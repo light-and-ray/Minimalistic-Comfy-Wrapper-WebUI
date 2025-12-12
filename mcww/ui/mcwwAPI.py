74638355e06f8819356b4f5a89327eb9f9100964
Andray <light.and.ray@gmail.com>
2025-12-12 21:14:25 +0400

    optimize queue indicator request a bit

--- mcww/ui/mcwwAPI.py
+++ mcww/ui/mcwwAPI.py
@@ -10,10 +10,23 @@ class API:
             "/mcww_api/queue_version",
             queueing.queue.getQueueVersion)
         self.app.add_api_route(
-            "/mcww_api/queue_indicator",
-            queueing.queue.getQueueIndicator)
-        self.app.add_api_route(
             "/mcww_api/outputs_version/{outputs_key}",
             queueing.queue.getOutputsVersion)
+
+        self.app.add_api_route(
+            "/mcww_api/queue_indicator",
+            self.getQueueIndicatorEndpoint)
         self.progressAPI = ProgressAPI(self.app)
+        self.lastQueueVersion = None
+        self.lastQueueIndicator = None
+
+
+    def getQueueIndicatorEndpoint(self):
+        version = queueing.queue.getQueueVersion()
+        if self.lastQueueVersion == version:
+            return self.lastQueueIndicator
+        else:
+            self.lastQueueVersion = version
+            self.lastQueueIndicator = queueing.queue.getQueueIndicator()
+            return self.lastQueueIndicator
 
