8d566600606f0547f7c602bec3be6c29babdec90
Andray <light.and.ray@gmail.com>
2025-12-13 16:35:49 +0400

    generalize apply changes mechanism in options ui

--- mcww/ui/misc/optionsUI.py
+++ mcww/ui/misc/optionsUI.py
@@ -1,23 +1,17 @@
 import gradio as gr
 from mcww import opts
+from mcww.utils import AttrDict
 
 
 class OptionsUI:
     def __init__(self):
+        self._components = AttrDict()
         self._buildOptionsUI()
 
 
-    @staticmethod
-    def _onApplyChanges(
-        accentColorHue,
-        maxQueueSize,
-        showToggleDarkLightButton,
-        showRunButtonCopy,
-    ):
-        opts.options.primaryHue = accentColorHue
-        opts.options.maxQueueSize = maxQueueSize
-        opts.options.showToggleDarkLightButton = showToggleDarkLightButton
-        opts.options.showRunButtonCopy = showRunButtonCopy
+    def _onApplyChanges(self, *args):
+        for key, value in zip(self._components.keys(), args):
+            setattr(opts.options, key, value)
         opts.saveOptions()
         gr.Info("Options saved, restart UI to apply some of them", 4)
 
@@ -25,30 +19,25 @@ class OptionsUI:
     def _buildOptionsUI(self):
         with gr.Column(visible=False, elem_classes=["options-main-column"]) as self.ui:
             with gr.Row(equal_height=True):
-                accentColorHue = gr.Slider(label=f"Accent color hue", show_reset_button=False,
+                self._components.primaryHue = gr.Slider(label=f"Accent color hue", show_reset_button=False,
                                     value=opts.options.primaryHue, minimum=0, maximum=360, step=1)
-                accentColorHuePreview = gr.ColorPicker(interactive=False, elem_classes=["accent-color-preview"],
+                primaryHuePreview = gr.ColorPicker(interactive=False, elem_classes=["accent-color-preview"],
                                     value=opts.getThemeColor(opts.options.primaryHue).c500, show_label=False, scale=0)
-                accentColorHue.change(
+                self._components.primaryHue.change(
                     fn=lambda hue: opts.getThemeColor(hue).c500,
-                    inputs=[accentColorHue],
-                    outputs=[accentColorHuePreview],
+                    inputs=[self._components.primaryHue],
+                    outputs=[primaryHuePreview],
                     show_progress='hidden',
                 )
-            maxQueueSize = gr.Slider(value=opts.options.maxQueueSize, minimum=10, maximum=999, step=1,
+            self._components.maxQueueSize = gr.Slider(value=opts.options.maxQueueSize, minimum=10, maximum=999, step=1,
                                                 show_reset_button=False, label="Max queue size")
-            showToggleDarkLightButton = gr.Checkbox(label='Show "☀️/🌙" button for changing dark/light theme',
+            self._components.showToggleDarkLightButton = gr.Checkbox(label='Show "☀️/🌙" button for changing dark/light theme',
                             value=opts.options.showToggleDarkLightButton)
-            showRunButtonCopy = gr.Checkbox(label='Show non-floating run button in workflow UI',
+            self._components.showRunButtonCopy = gr.Checkbox(label='Show non-floating run button in workflow UI',
                             value=opts.options.showRunButtonCopy)
 
             applyChanges = gr.Button(value="Apply changes", elem_classes=["mcww-save"])
             applyChanges.click(
                 fn=self._onApplyChanges,
-                inputs=[
-                    accentColorHue,
-                    maxQueueSize,
-                    showToggleDarkLightButton,
-                    showRunButtonCopy,
-                ]
+                inputs=list(self._components.values())
             )
