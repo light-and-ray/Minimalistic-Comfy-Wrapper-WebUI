62a47d3d77b8f6945e8d3a65a840bc879b82052d
Andray <light.and.ray@gmail.com>
2025-12-13 16:43:18 +0400

    improve options ui style

--- mcww/ui/misc/optionsUI.py
+++ mcww/ui/misc/optionsUI.py
@@ -18,23 +18,24 @@ class OptionsUI:
 
     def _buildOptionsUI(self):
         with gr.Column(visible=False, elem_classes=["options-main-column"]) as self.ui:
-            with gr.Row(equal_height=True):
-                self._components.primaryHue = gr.Slider(label=f"Accent color hue", show_reset_button=False,
-                                    value=opts.options.primaryHue, minimum=0, maximum=360, step=1)
-                primaryHuePreview = gr.ColorPicker(interactive=False, elem_classes=["accent-color-preview"],
-                                    value=opts.getThemeColor(opts.options.primaryHue).c500, show_label=False, scale=0)
-                self._components.primaryHue.change(
-                    fn=lambda hue: opts.getThemeColor(hue).c500,
-                    inputs=[self._components.primaryHue],
-                    outputs=[primaryHuePreview],
-                    show_progress='hidden',
-                )
-            self._components.maxQueueSize = gr.Slider(value=opts.options.maxQueueSize, minimum=10, maximum=999, step=1,
-                                                show_reset_button=False, label="Max queue size")
-            self._components.showToggleDarkLightButton = gr.Checkbox(label='Show "☀️/🌙" button for changing dark/light theme',
-                            value=opts.options.showToggleDarkLightButton)
-            self._components.showRunButtonCopy = gr.Checkbox(label='Show non-floating run button in workflow UI',
-                            value=opts.options.showRunButtonCopy)
+            with gr.Group():
+                with gr.Row(equal_height=True):
+                    self._components.primaryHue = gr.Slider(label=f"Accent color hue", show_reset_button=False,
+                                        value=opts.options.primaryHue, minimum=0, maximum=360, step=1)
+                    primaryHuePreview = gr.ColorPicker(interactive=False, elem_classes=["accent-color-preview"],
+                                        value=opts.getThemeColor(opts.options.primaryHue).c500, show_label=False, scale=0)
+                    self._components.primaryHue.change(
+                        fn=lambda hue: opts.getThemeColor(hue).c500,
+                        inputs=[self._components.primaryHue],
+                        outputs=[primaryHuePreview],
+                        show_progress='hidden',
+                    )
+                self._components.maxQueueSize = gr.Slider(value=opts.options.maxQueueSize, minimum=10, maximum=999, step=1,
+                                                    show_reset_button=False, label="Max queue size")
+                self._components.showToggleDarkLightButton = gr.Checkbox(label='Show "☀️/🌙" button for changing dark/light theme',
+                                value=opts.options.showToggleDarkLightButton)
+                self._components.showRunButtonCopy = gr.Checkbox(label='Show non-floating run button in workflow UI',
+                                value=opts.options.showRunButtonCopy)
 
             applyChanges = gr.Button(value="Apply changes", elem_classes=["mcww-save"])
             applyChanges.click(
