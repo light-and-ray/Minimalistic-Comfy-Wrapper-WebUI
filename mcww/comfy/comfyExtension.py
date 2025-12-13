b4527057d36b777ac554bf03d03512be6fa29101
Andray <light.and.ray@gmail.com>
2025-12-13 19:46:38 +0400

    fix comfy --listen flag detection

--- mcww/comfy/comfyExtension.py
+++ mcww/comfy/comfyExtension.py
@@ -14,7 +14,7 @@ def _initOpts():
     from comfy.cli_args import args
     portComfy = str(getattr(args, "port", "8188"))
     if not os.environ.get("GRADIO_SERVER_NAME"):
-        os.environ["GRADIO_SERVER_NAME"] = "0.0.0.0" if getattr(args, "listen", None) else "127.0.0.1"
+        os.environ["GRADIO_SERVER_NAME"] = "0.0.0.0" if "0.0.0.0" in getattr(args, "listen", "") else "127.0.0.1"
     opts.COMFY_ADDRESS = f"localhost:{portComfy}"
     portGradio = os.environ.get("GRADIO_SERVER_PORT", "7860")
     if portComfy == portGradio != "7861":
