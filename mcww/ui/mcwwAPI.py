ff3b8d729329ecb4b671fcb0048440a5f34f2e3f
Andray <light.and.ray@gmail.com>
2025-12-27 02:59:57 +0400

    use url_path_for to get path to pwa_icon; use opts.WEBUI_TITLE for name and move code into setUpPWA function

--- mcww/ui/mcwwAPI.py
+++ mcww/ui/mcwwAPI.py
@@ -1,29 +1,11 @@
+import os
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from fastapi.responses import Response
-from mcww import queueing
-from mcww.ui.uiUtils import pwaIconBytes
+from mcww import queueing, opts
+from mcww.utils import read_binary_from_file
+from mcww.ui.uiUtils import MCWW_WEB_DIR
 from mcww.ui.progressAPI import ProgressAPI
-
-PWA_MANIFEST = \
-{
-    "name": "Minimalistic Comfy Wrapper WebUI",
-    "icons": [
-        {
-            "src": "/pwa_icon.png",
-            "sizes": "1024x1024",
-            "type": "image/png",
-            "purpose": "maskable",
-        },
-        {
-            "src": "/pwa_icon.png",
-            "sizes": "1024x1024",
-            "type": "image/png",
-        }
-    ],
-    "start_url": "./",
-    "display": "standalone"
-}
 
 
 class API:
@@ -35,26 +17,13 @@ class API:
         self.app.add_api_route(
             "/mcww_api/outputs_version/{outputs_key}",
             queueing.queue.getOutputsVersion)
-
         self.app.add_api_route(
             "/mcww_api/queue_indicator",
             self.getQueueIndicatorEndpoint)
-        self.removeRoute('/pwa_icon')
-        self.app.add_api_route(
-            '/pwa_icon.png',
-            lambda: Response(content=pwaIconBytes, media_type="image/png"),
-            methods=["GET"],
-            name="pwa_icon",
-        )
-        self.removeRoute('/manifest.json')
-        self.app.add_api_route(
-            '/manifest.json',
-            lambda: PWA_MANIFEST,
-            methods=["GET"]
-        )
-        self.progressAPI = ProgressAPI(self.app)
         self.lastQueueVersion = None
         self.lastQueueIndicator = None
+        self.progressAPI = ProgressAPI(self.app)
+        self.setUpPWA()
 
 
     def getQueueIndicatorEndpoint(self):
@@ -72,3 +41,43 @@ class API:
             route for route in self.app.routes
             if not (isinstance(route, APIRoute) and route.path == path)
         ]
+
+
+    def setUpPWA(self):
+        pwaIconPath = os.path.join(MCWW_WEB_DIR, 'pwa_icon.png')
+        pwaIconBytes = read_binary_from_file(pwaIconPath)
+
+        self.removeRoute('/pwa_icon')
+        self.app.add_api_route(
+            '/pwa_icon.png',
+            lambda: Response(content=pwaIconBytes, media_type="image/png"),
+            methods=["GET"],
+            name="pwa_icon",
+        )
+
+        manifest =  {
+            "name": opts.WEBUI_TITLE,
+            "icons": [
+                {
+                    "src": self.app.url_path_for("pwa_icon"),
+                    "sizes": "1024x1024",
+                    "type": "image/png",
+                    "purpose": "maskable",
+                },
+                {
+                    "src": self.app.url_path_for("pwa_icon"),
+                    "sizes": "1024x1024",
+                    "type": "image/png",
+                }
+            ],
+            "start_url": "./",
+            "display": "standalone"
+        }
+
+        self.removeRoute('/manifest.json')
+        self.app.add_api_route(
+            '/manifest.json',
+            lambda: manifest,
+            methods=["GET"]
+        )
+
