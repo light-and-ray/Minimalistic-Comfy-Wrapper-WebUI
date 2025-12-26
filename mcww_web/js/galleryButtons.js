d7a38bdb3835618b042ff8fa2e115748bb160af2
Andray <light.and.ray@gmail.com>
2025-12-26 04:42:38 +0400

    fix isCameraActive for video component

--- mcww_web/js/galleryButtons.js
+++ mcww_web/js/galleryButtons.js
@@ -1,18 +1,5 @@
 
-// paste and camera buttons
-
-
-function isCameraActive() {
-    const imageContainers = document.querySelectorAll('.image-container');
-    for (container of imageContainers) {
-        const cameraButton = container.querySelector('button.camera-button.selected');
-        const video = container.querySelector('video');
-        if (video && uiElementIsVisible(video) && cameraButton) {
-            return true;
-        }
-    }
-    return false;
-}
+// paste button
 
 
 function fixClipboardPaste() {
@@ -35,13 +22,6 @@ function fixClipboardPaste() {
                 }
             }
         }
-        const cameraButton = container.querySelector('.source-selection > button:nth-of-type(2)');
-        if (cameraButton) {
-            if (!window.isSecureContext) {
-                cameraButton.style.display = "none";
-            }
-            cameraButton.classList.add("camera-button");
-        }
         container.dataset.clipboardFixAttached = "true";
     });
 }
@@ -52,15 +32,29 @@ onUiUpdate(fixClipboardPaste);
 // camera buttons
 
 
+function isCameraActive() {
+    const containers = document.querySelectorAll('.image-container, .video-container');
+    for (container of containers) {
+        const cameraButton = container.querySelector('button.camera-button.selected');
+        const video = container.querySelector('video');
+        if (video && uiElementIsVisible(video) && cameraButton) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 function fixCameraButtons() {
-    const imageContainers = document.querySelectorAll('.image-container, .video-container');
-    imageContainers.forEach(container => {
+    const containers = document.querySelectorAll('.image-container, .video-container');
+    containers.forEach(container => {
         if (container.dataset.cameraFixAttached) return;
         const cameraButton = container.querySelector('.source-selection > button:nth-of-type(2)');
         if (cameraButton) {
-        if (!window.isSecureContext || container.parentElement.classList.contains("no-camera")) {
+            if (!window.isSecureContext || container.parentElement.classList.contains("no-camera")) {
                 cameraButton.style.display = "none";
             }
+            cameraButton.classList.add("camera-button");
         }
         container.dataset.cameraFixAttached = "true";
     });
