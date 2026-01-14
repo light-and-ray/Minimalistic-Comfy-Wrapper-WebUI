397f42277ccabd4c1b15291159afce182b3d63b5
Andray <light.and.ray@gmail.com>
2026-01-15 00:03:38 +0400

    close screen recorder menu on escape

--- mcww_web/js/misc/hotkeys.js
+++ mcww_web/js/misc/hotkeys.js
@@ -29,7 +29,7 @@ document.addEventListener('keydown', (event) => {
     if (event.code === "Escape") {
         globalExitFullscreenIfExists();
         closeSidebarOnMobile();
-        clickVisibleButton(".click-on-escape, button.toast-close");
+        clickVisibleButton(".click-on-escape, button.toast-close, div.api-docs>div.backdrop");
     }
 
     if (document.activeElement.matches('textarea, input[type="text"], div.cm-content')) {
