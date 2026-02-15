98413890a3579c4173f8617b0f960c9fa4d646a2
Andray <light.and.ray@gmail.com>
2026-02-15 04:45:25 +0400

    refresh workflow on start only after api init

--- mcww/ui/mainUI.py
+++ mcww/ui/mainUI.py
@@ -129,9 +129,9 @@ class MinimalisticComfyWrapperWebUI:
             share_server_tls_certificate=os.environ.get("FRP_SHARE_SERVER_TLS_CERTIFICATE", None),
         )
         initClientID()
-        shared.projectUI.refreshWorkflows()
         shared.messages: Messages = Messages()
         shared.api: API = API(app)
+        shared.projectUI.refreshWorkflows()
         def debugPrintMessage(message):
             import json
             if message.get('type') not in ('progress_state'):
