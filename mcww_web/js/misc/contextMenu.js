4bae2a8921884840c7868294c394d355d1344dd7
Andray <light.and.ray@gmail.com>
2026-03-06 22:15:05 +0400

    use svg for link

--- mcww_web/js/misc/contextMenu.js
+++ mcww_web/js/misc/contextMenu.js
@@ -96,7 +96,13 @@ class McwwContextMenu {
             this.menu.appendChild(this.createItem('🡒', text, () => {
                 window.open(url, '_blank');
             }));
-            const item = this.createItem('⎘', 'Copy URL', () => {
+            const linkSvg = `
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39
+                            0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961"/>
+                </svg>
+                `
+            const item = this.createItem(linkSvg, 'Copy URL', () => {
                 copyTextToClipboard(url);
                 mouseAlert("Copied");
             });
