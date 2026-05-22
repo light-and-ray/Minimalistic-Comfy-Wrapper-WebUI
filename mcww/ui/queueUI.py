59fcd2ba0a0778688134f22f6d02c7996fde4844
Andray <light.and.ray@gmail.com>
2026-05-22 23:18:00 +0400

    decrease soft cancel message duration

--- mcww/ui/queueUI.py
+++ mcww/ui/queueUI.py
@@ -46,7 +46,7 @@ class QueueUI:
     def _getOnCancelBatchSoft(selectedId: int):
         def onCancelBatchSoft():
             queueing.queue.cancelBatchSoft(selectedId)
-            gr.Info("Soft canceled: The batch will stop after the current job finishes", 8)
+            gr.Info("Soft canceled: The batch will stop after the current job finishes", 6)
         return onCancelBatchSoft
 
     @staticmethod
