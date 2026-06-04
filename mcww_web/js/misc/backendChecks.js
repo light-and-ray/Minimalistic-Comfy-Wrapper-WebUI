4a06a78940e92abc846f55fe0999de1bb00845a0
Andray <light.and.ray@gmail.com>
2026-06-04 15:30:55 +0400

    adjust

--- mcww_web/js/misc/backendChecks.js
+++ mcww_web/js/misc/backendChecks.js
@@ -1,6 +1,6 @@
 
 const originalFetch = window.fetch;
-const BACKEND_CHECK_INTERVAL = 3000;
+const BACKEND_CHECK_INTERVAL = 2500;
 let g_backendNotAvailableInARow = 0;
 
 async function checkSameAppIdOnUiLoaded() {
