22e6d3256c27a18ce67b32bfd3db14ed6017c3e9
Andray <light.and.ray@gmail.com>
2026-06-20 22:01:45 +0400

    little fix

--- mcww_web/js/misc/hotkeys.js
+++ mcww_web/js/misc/hotkeys.js
@@ -47,7 +47,7 @@ document.addEventListener('keydown', (event) => {
         event.preventDefault();
     }
     if (event.code === "Escape") {
-        event.stopPropagation(); // block gradio's behavior when it select the first element in gallery on escape
+        event.stopPropagation(); // block gradio's behavior when it selects the first element in gallery on escape
     }
 
     if (isCtrl && event.code === "KeyL") {
