7326a5ac7f5740450ba206dd7b5adfa05d4a6bd0
Andray <light.and.ray@gmail.com>
2026-09-22 17:46:03 +0400

    little fix

--- comfy_web_dir/pwaCtrlW.js
+++ comfy_web_dir/pwaCtrlW.js
@@ -6,7 +6,7 @@ document.addEventListener('keydown', function(event) {
     if (!isRunningInPWA()) {
         return;
     }
-    if ((event.ctrlKey || event.metaKey) && event.code ==='KeyW') {
+    if ((event.ctrlKey || event.metaKey) && event.code === 'KeyW') {
         const closeTabButton = document.querySelector('.workflow-tabs-container .p-togglebutton.p-togglebutton-checked button.close-button');
         if (closeTabButton) {
             event.preventDefault();
